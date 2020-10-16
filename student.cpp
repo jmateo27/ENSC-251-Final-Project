20139169d32c5c951df1c76f4922bed04a4536f9
Danieva Paraiso <59851292+dparaiso@users.noreply.github.com>
2020-10-16 14:17:06 -0700

    Update student.cpp
    uh idk

--- student.cpp
+++ student.cpp
@@ -3,16 +3,16 @@
 
 //constructs
 Student::Student(string FName, string LName, float grade, int RScore, int id){
-    FirstName = FName; 
-    LastName = LName; 
+    FirstName = FName + ","; 
+    LastName = LName + ","; 
     CGPA = grade; 
     ResearchScore = RScore; 
     AppID = id; 
 }
 
 Student::Student(string FName, string LName, int id){
-    FirstName = FName; 
-    LastName = LName; 
+    FirstName = FName + ","; 
+    LastName = LName + ","; 
     AppID = id; 
 }
 
@@ -21,27 +21,48 @@ Student::Student(){
 }
 
 //set functions 
-void Student::set_FirstName(string FName){
-    FirstName = FName; 
+void Student::setFirstName(string FName){
+    FirstName = FName + ","; 
 }
 
-void Student::set_LastName(string LName){
-    LastName = LName;
+void Student::setLastName(string LName){
+    LastName = LName + ",";
 }
 
-void Student::set_CGPA(float grade){
+void Student::setCGPA(float grade){
     CGPA = grade; 
 }
 
-void Student::set_ResearchScore(int RScore){
+void Student::setResearchScore(int RScore){
     ResearchScore = RScore; 
 }
 
-void Student::set_AppID(int id){
+void Student::setAppID(int id){
     AppID = id; 
 }
 
+//getfunctions
+string Student::getFirstName(){
+    return FirstName; 
+}
 
+string Student::getLastName(){
+    return LastName; 
+}
+
+float Student::getCGPA(){
+    return CGPA; 
+}
+
+int Student::getRScore(){
+    return ResearchScore; 
+}
+
+int getid(){
+    return AppID;
+}
+
+//compare functions
 int compareCGPA(Student student1, Student student2){
     float CGPA1 = student1.getCGPA();
     float CGPA2 = student2.getCGPA();
@@ -51,4 +72,19 @@ int compareCGPA(Student student1, Student student2){
         return 1;
     else // CGPA1 < CGPA2
         return -1;
+}
+
+int compareFirstName (Student student1, Student student2, unsigned int len){
+    if (student1.get_FirstName() 
+}
+
+unsigned int findlen(string student1, string student2){
+    string name1 = student1.getFirstName();
+    string name2 = student2.getFirstName();
+    unsigned int i = 0;
+    while (name1[i] != (char)"," || name2[i] != (char)","){
+        i++;
+    }
+
+    return i;
 }
