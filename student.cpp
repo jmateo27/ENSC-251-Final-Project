423faf0cd4a4259d16503e4417747b2ec01d22bb
Danieva Paraiso <59851292+dparaiso@users.noreply.github.com>
2020-10-16 13:39:03 -0700

    Update student.cpp
    set functions

--- student.cpp
+++ student.cpp
@@ -1,6 +1,7 @@
 //student.cpp to implement your classes
 #include "student.hpp"
 
+//constructs
 Student::Student(string FName, string LName, float grade, int RScore, int id){
     FirstName = FName; 
     LastName = LName; 
@@ -17,4 +18,25 @@ Student::Student(string FName, string LName, int id){
 
 Student::Student(){
     //default constructor
+}
+
+//set functions 
+void Student::set_FirstName(string FName){
+    FirstName = FName; 
+}
+
+void Student::set_LastName(string LName){
+    LastName = LName;
+}
+
+void Student::set_CGPA(float grade){
+    CGPA = grade; 
+}
+
+void Student::set_ResearchScore(int RScore){
+    ResearchScore = RScore; 
+}
+
+void Student::set_AppID(int id){
+    AppID = id; 
 }
