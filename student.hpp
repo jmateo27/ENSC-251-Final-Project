63f195aad8733b46e0aebee84d60da47dc9b7a8a
jmateo27 <jmateo@sfu.ca>
2020-10-16 12:32:03 -0700

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -1,3 +1,15 @@
 //header file student.hpp to declare your classes
 using namespace std; //use namespace std
 #include <string> //you will have to use string in C++
+
+class Student{
+
+};
+
+class DomesticStudent public: Student{
+
+};
+
+class InternationalStudent public: Student{
+
+};
