9c9b122da940bfb9a0d4e380f8672c7c61fcf358
jmateo27 <jmateo@sfu.ca>
2020-10-16 12:36:01 -0700

    Update student.hpp
    added some comments

--- student.hpp
+++ student.hpp
@@ -5,14 +5,17 @@ using namespace std; //use namespace std
 #include <string> //you will have to use string in C++
 
 class Student{
+//parent class of the DomesticStudent and InternationalStudent classes
 
 };
 
 class DomesticStudent public: Student{
+//child class of the Student class
 
 };
 
 class InternationalStudent public: Student{
+//child class of the Student class
 
 };
 #endif //STUDENT_H
