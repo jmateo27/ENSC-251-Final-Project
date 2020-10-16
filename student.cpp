68086ada1f6e88f6f14fea8a10a51d5064173d66
Danieva Paraiso <59851292+dparaiso@users.noreply.github.com>
2020-10-16 13:34:28 -0700

    Update student.cpp
    constructs

--- student.cpp
+++ student.cpp
@@ -1,3 +1,20 @@
 //student.cpp to implement your classes
 #include "student.hpp"
 
+Student::Student(string FName, string LName, float grade, int RScore, int id){
+    FirstName = FName; 
+    LastName = LName; 
+    CGPA = grade; 
+    ResearchScore = RScore; 
+    AppID = id; 
+}
+
+Student::Student(string FName, string LName, int id){
+    FirstName = FName; 
+    LastName = LName; 
+    AppID = id; 
+}
+
+Student::Student(){
+    //default constructor
+}
